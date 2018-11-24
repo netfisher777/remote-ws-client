6afb045d71313b406993ef2a4c919f249aa706e0
netfisher777 <netfisher777@gmail.com>
2018-11-24 14:45:45 +0300

    refactoring

---
+++ remote-ws-client-service/src/main/java/com/magicvalleyworks/remotewsclient/service/wsfactories/SimpleWebServiceBeanFactory.java
@@ -0,0 +1,18 @@
+package com.magicvalleyworks.remotewsclient.service.wsfactories;
+
+import com.magicvalleyworks.remotewstarget.cbwc.factories.AbstractConsulBasedWebServiceFactory;
+import com.magicvalleyworks.remotewstarget.wsfacadeapi.SimpleWebService;
+import com.magicvalleyworks.remotewstarget.wsfacadeapi.SimpleWebService_Service;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class SimpleWebServiceBeanFactory extends AbstractConsulBasedWebServiceFactory<SimpleWebService, SimpleWebService_Service> {
+    @Produces
+    @ApplicationScoped
+    @Override
+    public SimpleWebService createWebService() {
+        return createPort(new SimpleWebService_Service(), SimpleWebService.class, "SimpleWebService");
+    }
+}
